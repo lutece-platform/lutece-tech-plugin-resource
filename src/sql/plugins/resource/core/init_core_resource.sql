886c878314b829ad7f866dc19ba2e41607a680a1
AlexandreClose <AlexandreClose@users.noreply.github.com>
2016-11-08 09:10:41 +0000

    Fix the INSERT INTO sql command for core admin right
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-resource@85874 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/resource/core/init_core_resource.sql
+++ src/sql/plugins/resource/core/init_core_resource.sql
@@ -1,4 +1,4 @@
-INSERT INTO core_admin_right VALUES ('RESOURCE_MANAGE_RESOURCES', 'resource.resourceManagement.pageTitle', 2, 'jsp/admin/plugins/resource/ManageResources.jsp', 'resource.resourceManagement.description', 0, 'resource', 'CONTENT', NULL, NULL, NULL);
+INSERT INTO core_admin_right (id_right, name, level_right, admin_url, description, is_updatable, plugin_name, id_feature_group, icon_url, documentation_url, id_order) VALUES ('RESOURCE_MANAGE_RESOURCES', 'resource.resourceManagement.pageTitle', 2, 'jsp/admin/plugins/resource/ManageResources.jsp', 'resource.resourceManagement.description', 0, 'resource', 'CONTENT', NULL, NULL, NULL);
 INSERT INTO core_user_right (id_right,id_user) VALUES ('RESOURCE_MANAGE_RESOURCES',1);
 
 INSERT INTO core_datastore VALUES ( 'core.cache.status.resource.resourceCacheService.enabled', '1' );
