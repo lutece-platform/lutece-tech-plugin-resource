a2cb62bf6ac93b43e518e0fe2269ccf533a55e4c
rdeniel <renaud.deniel@outlook.com>
2022-02-03 17:55:01 +0100

    RESOURCE-15 add a unit test

---
+++ src/test/java/fr/paris/lutece/plugins/resource/business/ResourceTypeDefaultImplementationTest.java
@@ -0,0 +1,14 @@
+package fr.paris.lutece.plugins.resource.business;
+
+import org.junit.Test;
+
+import fr.paris.lutece.test.LuteceTestCase;
+
+public class ResourceTypeDefaultImplementationTest extends LuteceTestCase
+{
+    @Test
+    public void testResourceTypeDefaultImplementations( )
+    {
+        assertTrue( true );
+    }
+}
