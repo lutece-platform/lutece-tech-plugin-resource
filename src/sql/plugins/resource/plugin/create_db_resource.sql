9cf4a0c70eabe0999b6f186093896cce3f433e5f
Rafik YAHIAOUI <rafik.yahiaoui@paris.fr>
2026-08-25 18:22:55 +0200

    lutece:liquibase-sql

--- src/sql/plugins/resource/plugin/create_db_resource.sql
+++ src/sql/plugins/resource/plugin/create_db_resource.sql
@@ -1,3 +1,6 @@
+-- liquibase formatted sql
+-- changeset resource:create_db_resource.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 DROP TABLE IF EXISTS resource_resource;
 CREATE TABLE resource_resource
 (
